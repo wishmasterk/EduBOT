b31f5f48d82ae7560bcaab925f9b1c17bb1eeb62
wishmasterk <keshavagarwal7505@gmail.com>
2025-11-21 11:36:11 +0530

    replaced deprecated packages

--- rag.py
+++ rag.py
@@ -8,7 +8,7 @@ from pdf2image import convert_from_path
 from dotenv import load_dotenv
 from langchain_core.documents import Document
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter 
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 
